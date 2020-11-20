8f62b60def1481233cb67136bba4d343165d502a
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-11-20 10:41:10 +0100

    Compile all in playground

--- rollup.config.ts
+++ rollup.config.ts
@@ -10,7 +10,7 @@ const pkg = require('./package.json');
 export default {
   input: `src/**/*.ts`,
   output: { dir: 'dist', format: 'es', sourcemap: true },
-  external: [...Object.keys(pkg.dependencies), /@material/, /rxjs/],
+  external: [],
   plugins: [
     multiInput(),
     json(),
