4fe98616d9e7f16e20685987ae8f10e88f0de67a
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-04 17:55:32 +0100

    Select active dna

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,4 @@ export * from './elements/dht-stats';
 export * from './elements/entry-contents';
 export * from './elements/entry-graph';
 export * from './elements/source-chain';
+export * from './elements/select-active-dna';
